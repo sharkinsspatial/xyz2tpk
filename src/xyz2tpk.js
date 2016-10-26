2db27869171de67b9971a75344c62049771f405b
sharkinsspatial <sharkinsgis@gmail.com>
2016-10-26 11:54:48 -0300

    Added zip functionality.

--- src/xyz2tpk.js
+++ src/xyz2tpk.js
@@ -5,13 +5,14 @@ import Sphericalmercator from 'sphericalmercator';
 import tilelive from 'tilelive';
 import tileliveArcGIS from 'tilelive-arcgis';
 import tileliveHttp from 'tilelive-http';
+import zipFolder from 'zip-folder';
 
 const DOMParser = xmldom.DOMParser;
-const sphericalmercator = new Sphericalmercator();
 
 export function writeConf(minzoom, maxzoom, extension, directory, callback) {
     const confPath = path.resolve(__dirname, '..', 'templateConf.xml');
     fs.readFile(confPath, (err, data) => {
+        if (err) callback(err);
         const doc = new DOMParser().parseFromString(data.toString('utf-8'));
         const levelids = doc.getElementsByTagName('LevelID');
         const nodesToRemove = [];
@@ -35,6 +36,8 @@ export function writeConf(minzoom, maxzoom, extension, directory, callback) {
 export function writeBounds(bounds, directory, callback) {
     const confPath = path.resolve(__dirname, '..', 'templateConf.cdi');
     fs.readFile(confPath, (err, data) => {
+        if (err) callback(err);
+        const sphericalmercator = new Sphericalmercator();
         const mercatorBounds = sphericalmercator.convert(bounds, '900913');
         const doc = new DOMParser().parseFromString(data.toString('utf-8'));
         doc.getElementsByTagName('XMin')[0].textContent = mercatorBounds[0];
@@ -51,12 +54,13 @@ export function writeBounds(bounds, directory, callback) {
 
 export function xyz2tpk(bounds, minzoom, maxzoom, directory, token, callback) {
     // Register sources with tilelive
-    tileliveHttp(tilelive);
+    tileliveHttp(tilelive, { retry: true });
     tileliveArcGIS.registerProtocols(tilelive);
 
     const options = {
         type: 'scanline',
         close: 'true',
+        timeout: 100000000,
         bounds,
         minzoom,
         maxzoom
@@ -66,9 +70,19 @@ export function xyz2tpk(bounds, minzoom, maxzoom, directory, token, callback) {
     const arcgisTemplate = `arcgis://${directory}`;
     const copy = () => {
         tilelive.copy(httpTemplate, arcgisTemplate, options, (err) => {
-            if (err) throw err;
+            if (err) callback(err);
             writeBounds(bounds, directory, callback);
         });
     };
     writeConf(minzoom, maxzoom, extension, directory, copy);
+}
+
+export function ziptpk(directory, callback) {
+    const tmpDirectory = path.resolve(directory, '..');
+    const name = path.basename(directory);
+    const zipFile = path.resolve(tmpDirectory, `${name}.tpk`);
+    zipFolder(directory, zipFile, (err) => {
+        if (err) callback(err);
+        callback();
+    });
 }
--- test/xyz2tpk.test.js
+++ test/xyz2tpk.test.js
@@ -3,15 +3,32 @@ import fs from 'fs';
 import path from 'path';
 import rimraf from 'rimraf';
 import xmldom from 'xmldom';
-import { writeBounds, writeConf } from '../src/xyz2tpk';
+import mkdirp from 'mkdirp';
+import { writeBounds, writeConf, ziptpk } from '../src/xyz2tpk';
 import box from './testBox';
 
 const DOMParser = xmldom.DOMParser;
-const testtemp = path.join(__dirname, '/testtmp');
+const tpkName = 'tpk';
+const testtpk = path.join(__dirname, '/testtmp', tpkName);
+const testtemp = path.dirname(testtpk);
 
 test('setup', (t) => {
-    fs.mkdirSync(testtemp);
+    mkdirp(testtpk);
     t.end();
+});
+
+test('writeConf', (t) => {
+    t.plan(1);
+    const minzoom = 2;
+    const maxzoom = 4;
+    writeConf(minzoom, maxzoom, null, testtpk, () => {
+        const confPath = path.join(testtpk, 'Conf.xml');
+        fs.readFile(confPath, (readErr, data) => {
+            const doc = new DOMParser().parseFromString(data.toString('utf-8'));
+            const levelids = doc.getElementsByTagName('LevelID');
+            t.equals(levelids.length, 3);
+        });
+    });
 });
 
 test('writeBounds', (t) => {
@@ -21,36 +38,29 @@ test('writeBounds', (t) => {
     const ymax = '3858273.658236698';
 
     t.plan(4);
-    writeBounds(box, testtemp, () => {
-        const confPath = path.join(testtemp, 'Conf.cdi');
+    writeBounds(box, testtpk, () => {
+        const confPath = path.join(testtpk, 'Conf.cdi');
         fs.readFile(confPath, (readErr, data) => {
             const doc = new DOMParser().parseFromString(data.toString('utf-8'));
             t.equals(doc.getElementsByTagName('XMin')[0].textContent, xmin);
             t.equals(doc.getElementsByTagName('YMin')[0].textContent, ymin);
             t.equals(doc.getElementsByTagName('XMax')[0].textContent, xmax);
             t.equals(doc.getElementsByTagName('YMax')[0].textContent, ymax);
-            rimraf(testtemp, () => {
-            });
         });
     });
 });
 
-test('writeConf', (t) => {
+test('ziptpk', (t) => {
     t.plan(1);
-    const minzoom = 2;
-    const maxzoom = 4;
-    writeConf(minzoom, maxzoom, null, testtemp, () => {
-        const confPath = path.join(testtemp, 'Conf.xml');
-        fs.readFile(confPath, (readErr, data) => {
-            const doc = new DOMParser().parseFromString(data.toString('utf-8'));
-            const levelids = doc.getElementsByTagName('LevelID');
-            t.equals(levelids.length, 3);
+    const tpkfile = path.resolve(testtemp, tpkName);
+    ziptpk(testtpk, () => {
+        fs.stat(tpkfile, (err) => {
+            t.error(err);
         });
     });
 });
 
-test('teardown', (t) => {
+test.onFinish(() => {
     rimraf(testtemp, () => {
-        t.end();
     });
 });
